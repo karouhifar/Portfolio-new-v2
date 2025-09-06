84f8fd798b066e5d11b2bbc36a093da792f55ebf
karouhifar <karouhifar@gmail.com>
2025-09-06 16:29:59 -0400

    added initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import Hero from "@/components/Hero/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Welcome to My Portfolio</h1>
-        <p>Modern Fullstack Developer And Cloud Engineer.</p>
+    <main
+      className="relative flex justify-center items-center 
+      h-screen flex-col overflow-hidden mx-auto sm:px-10 px-5"
+    >
+      <div className="w-full max-w-7xl">
+        <Hero />
       </div>
     </main>
   );
