c4d7795a576c801869ed17dc3da046f717da6615
karouhifar <karouhifar@gmail.com>
2025-09-06 15:41:55 -0400

    implemented inital stage of nextjs

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>Modern Fullstack Developer And Cloud Engineer.</p>
+      </div>
+    </main>
+  );
+}
