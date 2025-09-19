5b5591ff27c0d92f804a3840a2fe6ad38d89460a
karouhifar <karouhifar@gmail.com>
2025-09-19 03:10:19 -0400

    updated fixes

--- components/Story/Story.tsx
+++ components/Story/Story.tsx
@@ -12,7 +12,6 @@ const Story: React.FC = () => {
           // Control hub size and border thickness with CSS vars for easy tuning
           style={
             {
-              // @ts-expect-errore custom properties
               "--hub-size": "180px",
               "--hub-ring": "14px",
             } as React.CSSProperties
