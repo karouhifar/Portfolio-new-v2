913da93f3c7364b5d3fd7b51209872fe0a0d5f06
karouhifar <karouhifar@gmail.com>
2026-04-24 02:11:22 -0400

    refactor: comment out unused Resend email sending logic in API route

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from "@/components/Email/EmailTemplate";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 // This will be called when you GET /api/send
 export async function GET() {
@@ -17,27 +17,27 @@ export async function POST(req: NextRequest) {
     // (optional) read data from request body, e.g. contact form submission
     const body = await req.json();
     const { toEmail, firstName, message, subject } = body;
-    const { data, error } = await resend.emails.send({
-      from: `Kamyab Rouhifar <${process.env.FROM_EMAIL as string}>`, // MUST be a verified sender/domain
-      to: [toEmail], // test inbox Resend provides
-      subject: subject ?? "Hello from Next.js + Resend",
-      // Option A: use React template
-      react: EmailTemplate({
-        firstName: firstName ?? "Friend",
-        message: message ?? "It works! 🎉",
-      }),
-    });
+    // const { data, error } = await resend.emails.send({
+    //   from: `Kamyab Rouhifar <${process.env.FROM_EMAIL as string}>`, // MUST be a verified sender/domain
+    //   to: [toEmail], // test inbox Resend provides
+    //   subject: subject ?? "Hello from Next.js + Resend",
+    //   // Option A: use React template
+    //   react: EmailTemplate({
+    //     firstName: firstName ?? "Friend",
+    //     message: message ?? "It works! 🎉",
+    //   }),
+    // });
 
-    if (error) {
-      return NextResponse.json({ ok: false, error }, { status: 500 });
-    }
+    // if (error) {
+    //   return NextResponse.json({ ok: false, error }, { status: 500 });
+    // }
 
-    return NextResponse.json({ ok: true, data });
+    return NextResponse.json({ ok: true });
   } catch (err: unknown) {
     console.error(err);
     return NextResponse.json(
       { ok: false, error: "Unexpected error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
